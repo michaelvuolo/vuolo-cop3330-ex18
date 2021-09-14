826c2e49fc39114239dffb28dfe13a07d5641702
michaelvuolo <michaelvuolo1@gmail.com>
2021-09-13 20:51:35 -0400

    init

---
+++ src/main/java/edu/ucf/exercise.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Michael Vuolo
+ */
+
+package edu.ucf;
+
+import java.util.Scanner;
+
+public class exercise {
+    public static void main(String[] args) {
+        
+    }
+}
